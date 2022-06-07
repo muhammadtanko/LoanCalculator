9adb63f090275fa508b3ed49ca6753f6211aae53
muhammad-tanko <teekay014@gmail.com>
2022-06-07 10:16:16 +0100

    task: second commit

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// let events = require ("events");
-
-let Calculator = require("./loan-calc");
-
-let myCalc = new Calculator();
-
-console.log(myCalc.LoanCalc(1000000, 20, 70));
+module.exports.LoanCalc = require("./loan-calc");
